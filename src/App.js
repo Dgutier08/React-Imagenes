5073eb228b655f8bec6e8ab8973a5ee501521f00
Dgutier08 <102337196+Dgutier08@users.noreply.github.com>
2022-08-26 16:14:31 -0400

    Desafio-react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import header from './components/header';
+
+import Card from "react-bootstrap/card";
+import Button from "react-bootstrap/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export const App = () => {
+  return (
+    <card/>
+  )
+};
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css"; // Importamos CSS
+import Button from "react-bootstrap/Button";
+
+import React from 'react';
+function App() {
+return (
+<h1> Galeria de Imagenes con react</h1>
+);
+}
+export default App;
